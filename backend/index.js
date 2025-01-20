d98ef772778fa92b811e468a2ca889c268b7999e
Qarib Iqbal <qaribiqbal92@gmail.com>
2025-01-20 16:45:08 +0500

    Added API

--- backend/index.js
+++ backend/index.js
@@ -17,16 +17,19 @@ const connection = mysql.createConnection(
     }
 )
 connection.connect((err) => {
-     if (err)
-         {
-             throw err;
-              return; }
-      console.log(`Connected to MySQL Server`); 
-    });
-    if(connection)
-    {
-        console.log (connection);
+    if (err) {
+        throw err;
+        return;
     }
+    console.log(`Connected to MySQL Server`);
+});
+if (connection.state === 'disconnected') {
+    connection.connect();
+}
+
+if (connection) {
+    console.log(connection);
+}
 // if(connection){
 //     connection.query('SELECT * FROM Client', (err, results) => {
 //         if (err) {
@@ -37,44 +40,82 @@ connection.connect((err) => {
 //       });
 //     }
 
-    app.get('/clients',(req,res)=>{
+app.get('/clients', (req, res) => {
     //const response =req.response;
-    connection.query('SELECT * FROM Client',(err,results)=>{
-        if(err)
-        {
+    connection.query('SELECT * FROM Client', (err, results) => {
+        if (err) {
             console.error('Error executing query:', err.message);
             res.status(500).json({ error: 'Error fetching data' });
             return;
         }
-        else{
-            
-            console.log('Query results:', results);
-            res.status(200).json(results);  
-        }
-    })
-     });
+        else {
 
-     app.post('/addAppliance',(req,res)=>
-    {
-        const {contactNo, name, address,serial, companyName, Type, dateOfArrival }=req.body;
-        if(!contactNo|| !name|| !address|| !serial || !companyName || !Type || !dateOfArrival)
-        {
-            console.log(contactNo);
-            return res.status(404).send('Missing parameters');
+            console.log('Query results:', results);
+            res.status(200).json(results);
         }
-        else{
-            const query = 'Call AddApplianceAndClient(?,?,?,?,?,?,?)';
-            connection.query(query,[contactNo, name, address,serial, companyName, Type, dateOfArrival],(err,result)=>
-            {
-            if(err)
-                {
-                    console.log(err.message);
-                    return res.status(500).send('error callling stored procedure');
-                }    
-                console.log('client added' ,res);
-                return res.status(200).send('client and appilance added successfully!');
-            }
-        )
-    }
     })
-     // connection.close();
+});
+app.post('/addCentre', (req, res) => {
+    const { Cid } = req.body;
+    if (!Cid) {
+        console.log(Cid);
+        res.status(404).send("Centre Id can't be empty");
+    }
+    else {
+        try {
+            connection.query(`insert into Centre (Cid) values (Cid);`);
+            res.status(200).send(Cid);
+        }
+        catch (e) {
+            console.log(e.message);
+            res.status(500).send('Failed Query!');
+        }
+    }
+
+})
+
+app.post('/addStore', (req, res) => {
+    const { Sid } = req.body;
+    if (!Sid) {
+        console.log(Sid);
+        res.status(404).send("Store Id can't be empty");
+    }
+    else {
+        try {
+            connection.query('Insert into Store values (Sid);');
+            res.status(200).send(Sid);
+        }
+        catch (e) {
+            console.log(e.message);
+        }
+    }
+})
+
+//  app.get('/Appliances',(req,res)=>
+// {
+//     connect.query('select a.* from appliance as a join Centre as c on a.Cid==a.Cid')
+// })
+app.post('/addAppliance', (req, res) => {
+    const { contact_No, name, address, serial_No, companyName, Type, dateOfArrival, Sid, Cid } = req.body;
+    if (!contact_No || !name || !address || !serial_No || !companyName || !Type || !dateOfArrival || !Sid || !Cid) {
+        console.log(contact_No);
+        return res.status(404).send('Missing parameters');
+    }
+    else {
+        const query = 'Call AddApplianceAndClient(?,?,?,?,?,?,?,?,?)';
+        try {
+
+            connection.query(query, [contact_No, name, address, serial_No, companyName, Type, dateOfArrival, Sid, Cid])
+            console.log('client added', res);
+            return res.status(200).send('client and appilance added successfully!');
+        }
+        catch (e) {
+            console.log(err.message);
+            return res.status(500).send('error callling stored procedure');
+        }
+    }
+
+}
+)
+
+//    connection.close();
